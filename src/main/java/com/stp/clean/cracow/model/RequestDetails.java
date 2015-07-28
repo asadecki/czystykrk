af50c310fa1c6649306a2f69c7b778cf25a8c558
Aleksander Sadecki <aleksander.sadecki@schibsted.pl>
2015-07-28 11:12:39 +0200

    dupa dupa dupa cycki gowno

--- src/main/java/com/stp/clean/cracow/model/RequestDetails.java
+++ src/main/java/com/stp/clean/cracow/model/RequestDetails.java
@@ -2,7 +2,6 @@ package com.stp.clean.cracow.model;
 
 import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.NotNull;
 import javax.ws.rs.QueryParam;
 import java.util.List;
 
@@ -12,51 +11,71 @@ public class RequestDetails {
     @QueryParam("userName")
     private String userName;
 
-    @NotNull
+    @QueryParam("email")
+    private String email;
+
+    @QueryParam("phoneNumber")
+    private String phoneNumber;
+
     @QueryParam("latitude")
     private Double latitude;
 
-    @NotNull
     @QueryParam("longitude")
     private Double longitude;
 
-    @NotNull
-    @QueryParam("photoUrls")
-    private List<String> photoUrls;
+    @QueryParam("photos")
+    private List<String> photos;
+
 
     public RequestDetails() {
         // Jackson deserialization
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public void setLatitude(Double latitude) {
-        this.latitude = latitude;
-    }
-
-    public void setLongitude(Double longitude) {
-        this.longitude = longitude;
-    }
-
-    public void setPhotoUrls(List<String> photoUrls) {
-        this.photoUrls = photoUrls;
     }
 
     public String getUserName() {
         return userName;
     }
 
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
     public Double getLatitude() {
         return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
     }
 
     public Double getLongitude() {
         return longitude;
     }
 
-    public List<String> getPhotoUrls() {
-        return photoUrls;
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public List<String> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<String> photos) {
+        this.photos = photos;
     }
 }
